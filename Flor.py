4f369b572d7921e0bed3faf4fab8bf4a321d88a7
RoyGeova07 <royumana3@gmail.com>
2024-10-04 00:13:03 -0600

    Practica Libreria Turtle

---
+++ Flor.py
@@ -0,0 +1,72 @@
+from turtle import *
+from math import *
+
+speed(0)
+bgcolor("black")
+goto(0, -40)
+
+# Aquí se dibuja la flor
+for i in range(16):
+    for j in range(16):
+        color('#FFA216')
+        rt(90)
+        circle(150 - j * 6, 90)
+        lt(90)
+        circle(150 - j * 6, 90)
+        rt(180)
+    circle(40, 24)
+
+color('black')
+shape('circle')
+shapesize(0.5)
+fillcolor('#8B4513')
+golden_ang = 137.508
+phi = golden_ang * (pi / 180)
+
+for i in range(140):
+    r = 4 * sqrt(i)
+    theta = i * phi
+    x = r * cos(theta)
+    y = r * sin(theta)
+    penup()
+    goto(x, y)
+    setheading(i * golden_ang)
+    pendown()
+    stamp()
+
+def circulo(x, y):
+    penup()
+    goto(x, y)
+    pendown()
+    color('black')
+    fillcolor('#FFA216')
+    begin_fill()
+    circle(3)
+    end_fill()
+
+def Dibujar_R(x, y):
+    Posicion_R = [(x,y),(x,y+20),(x+12,y+20),(x+12,y+10),
+                  (x,y+10),(x,y),(x+12,y),(x+12,y+5)]
+
+    for pos in Posicion_R:
+        circulo(*pos)
+
+def Dibujar_O(x,y):
+    circulo(x+12,y+10)
+    
+def Dibujar_Y(x,y):
+    Posicion_Y = [(x,y+20),(x+10,y+10),(x+20,y+20),
+                  (x+10,y+10),(x+10,y),(x+20,y),(x+10,y)]
+    
+    for pos in Posicion_Y:
+        circulo(*pos)
+    
+
+# Llamadas a las funciones para dibujar letras centradas
+# Ajuste de las posiciones para centrar el nombre dentro de la flor
+Dibujar_R(-60, 30)  # Ajuste la posición de la "R"
+Dibujar_O(-10, 30)  # Ajuste la posición de la "O"
+Dibujar_Y(40, 30)   # Ajuste la posición de la "Y"
+
+hideturtle()
+done()
